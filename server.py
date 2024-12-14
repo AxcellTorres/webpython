763a7e38645fe13865ce2c90ecdf7408d826bcce
Kobayashi <157777154+AxcellTorres@users.noreply.github.com>
2024-12-14 09:56:54 -0700

    Log in

--- server.py
+++ server.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/Login')
+@app.route('/Menu')
 
 def home():
-    return render_template('index.html')
+    return render_template('Login.html')
+
+def menu():
+    return render_template('Menu.html')
